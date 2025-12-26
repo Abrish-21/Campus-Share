1f0550873cbcfc46192d4e66b4835089ffdc6d5d
Adey <adeyebuy2@gmail.com>
2025-12-26 13:37:48 +0300

    Fix cors errors

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://campus-share-backend.onrender.com',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api/v1') // Remove '/api' prefix
       }
